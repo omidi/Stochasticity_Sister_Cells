7a2c6154d7975be96830310a377c7235d3188b0d
omidi <saeed.omidi@gmail.com>
2016-06-01 15:45:54 +0200

    fitHMM

--- state_rate_matrix.py
+++ state_rate_matrix.py
@@ -3,6 +3,8 @@ from scipy.linalg import expm
 from scipy.stats import norm
 from constants import *
 import scipy.optimize as optimize
+import csv
+import os
 
 def state_rate_matrix(k_ON, k_OFF, k_s, k_d, p_max):
     I = np.identity(p_max)
@@ -53,9 +55,6 @@ def log_likelihood_for_model(D, Po, Pt, P0):
 
 
 def log_likelihood(D, x):
-    p_max = 250
-    delta_time = 5
-    deg_rate = .00456
     Po = observation_matrix(signal, alpha, beta, sigma_b2, p_max)
     M = state_rate_matrix(x[0], x[1], x[2], deg_rate, p_max)
     # M = state_rate_matrix(.5, .5, 2, 0.0145, p_max)
@@ -64,61 +63,70 @@ def log_likelihood(D, x):
     logL = log_likelihood_for_model(signal, Po, Pt, P0)
     return logL
 
+def arguments():
+    """adding arguments for the script"""
+    import argparse
+    parser = argparse.ArgumentParser(description="""
+    Take an input file that contains all the data, the column for the cell that is
+    interested, and it tries to fit the parameters for the birth-death process.
+    """)
+    parser.add_argument('-i', '--input', dest='input', action='store',
+                        type=str, required=True,
+                        help='The input file, should be in CSV format')
+    parser.add_argument('-c', '--column', dest='column', action='store',
+                        type=int, required=True,
+                        help="""The column for the cell. The first column is 1.""")
+    parser.add_argument('-d', '--deg_rate', dest='deg_rate', action='store',
+                        type=float, required=True,
+                        help="""The mRNA degradation rate for the clone.""")
+    parser.add_argument('-o', '--output', dest='dest', action='store',
+                        type=str, required=False, default="",
+                        help="""The destination directory""")
+    args = parser.parse_args()
+    return args
 
+if __name__ == '__main__':
+    args = arguments()
+    signal = []
+    time = []
+    args.column -= 1
+    delta_time = 5
+    t = 0
+    with open(args.input, 'rU') as inf:
+        for rec in csv.reader(inf, dialect=csv.excel_tab, delimiter=','):
+            if rec[args.column] == "":
+                break
+            signal.append(float(rec[args.column]))
+            time.append(t)
+            t += delta_time
+    signal = np.array(signal)
+    time = np.array(time)
+    deg_rate = args.deg_rate
+    T = len(time)
+    p_max = int(max(signal)/alpha) + 30
+    res_file_name = os.path.join(args.dest, "cell_%d_%s" % (args.column+1, os.path.basename(args.input)))
+    res_file = open(res_file_name, 'w')
+    res_file.write('\t'.join(['k_ON', 'k_OFF', 'k_s', 'deg', 'logL\n']))
+    def objective(x):
+        print x
+        if(x[0] > 5 or x[0] < 0):
+            return np.Inf
+        if(x[1] > 5 or x[1] < 0):
+            return np.Inf
+        if(x[2] > 10 or x[2] < 0):
+            return np.Inf
+        logL = log_likelihood(signal, x)
+        print logL
+        res_file.write('\t'.join([
+            str(x[0]),
+            str(x[1]),
+            str(x[2]),
+            str(deg_rate),
+            str(logL) + '\n',
+        ]))
+        return -logL
 
-import csv
-# T = 100
-# numb = np.zeros(T, np.int)
-# signal = np.zeros(T, dtype=np.float64)
-# time = np.zeros(T, dtype=np.int)
-# state = np.zeros(T, dtype=np.int)
-# with open("data.simulated") as inf:
-#     for i, rec in enumerate(csv.DictReader(inf, delimiter='\t')):
-#         if i>=T:
-#             break
-#         numb[i] = int(rec["numb"])
-#         signal[i] = float(rec["signal"])
-#         time[i] = int(rec["time"])
-#         state[i] = int(rec["states"])
-
-input = "4.11 with mitosis.csv"
-column = 90
-column -= 90
-signal = []
-time = []
-delta_t = 5
-t = 0
-with open(input, 'rU') as inf:
-    for rec in csv.reader(inf, dialect=csv.excel_tab, delimiter=','):
-        if rec[column] == "":
-            break
-        signal.append(float(rec[column]))
-        time.append(t)
-        t += delta_t
-signal = np.array(signal)
-time = np.array(time)
-T = len(time)
-
-
-def objective(x):
-    print x
-    if(x[0] > 5 or x[0] < 0):
-        return np.Inf
-    if(x[1] > 5 or x[1] < 0):
-        return np.Inf
-    if(x[2] > 10 or x[2] < 0):
-        return np.Inf
-    logL = log_likelihood(signal, x)
-    print logL
-    # res_file.write('\t'.join([
-    #     str(x[0]),
-    #     str(x[1]),
-    #     str(x[2]),
-    #     str(deg_rate),
-    #     str(logL) + '\n',
-    # ]))
-    return -logL
-
-x = [1./10, 1./5, 3.563]
-print objective(x)
-res = optimize.fmin(objective, x)
+    x = [1./2, 1./2, 3.]
+    print objective(x)
+    res = optimize.fmin(objective, x)
+    res_file.close()
